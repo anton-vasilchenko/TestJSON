c8f4003172993eba8bc6901e11f8bae87522ce75
a.vasilchenko <anton.miet@gmail.com>
2021-01-29 22:21:10 +0300

    Initial Commit

---
+++ TestJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestJSON
+//
+//  Created by Антон Васильченко on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
